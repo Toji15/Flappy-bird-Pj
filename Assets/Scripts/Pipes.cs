fe42ee9a78392f0764dec5e47c2a92d4d6383cd5
Toji15 <l3e.zenin@gmail.com>
2024-04-16 09:09:30 -0400

    Initial commit

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+   public float speed;
+    // Start is called before the first frame update
+    void Start() {
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move pipes left
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+        
+    }
+    private void FunctionExample()
+    {
+        //
+    }
+}
